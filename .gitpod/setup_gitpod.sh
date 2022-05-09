9770914c4469347b75591fc6b5f915910a898622
Merith <merith.tk@gmail.com>
2022-05-09 05:22:27 +0000

    fix gitpod config

--- .gitpod/setup_gitpod.sh
+++ .gitpod/setup_gitpod.sh
@@ -1,8 +1,14 @@
 git submodule update --init --recursive
 
+
+if [ ! -d ~/.gradle/ ]; then
+    mkdir ~/.gradle/
+fi
 if [ $GPR_USER ]; then
     echo "gpr.user = $GPR_USER" > ~/.gradle/gradle.properties
+    echo "Wrote GPR_USER to ~/.gradle/gradle.properties"
 fi
 if [ $GPR_KEY ]; then
-    echo "gpr.key = $GPR_KEY" > ~/.gradle/gradle.properties
+    echo "gpr.key = $GPR_KEY" >> ~/.gradle/gradle.properties
+    echo "Wrote GPR_KEY to ~/.gradle/gradle.properties"
 fi
