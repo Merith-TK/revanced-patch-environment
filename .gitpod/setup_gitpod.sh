fa26da12b2277e52aaaff38ab237f8b054ef7e9d
Merith <merith.tk@gmail.com>
2022-05-09 05:19:16 +0000

    Setup script and revanced/patcher
    mend

--- .gitpod/setup_gitpod.sh
+++ .gitpod/setup_gitpod.sh
@@ -1,1 +1,8 @@
 git submodule update --init --recursive
+
+if [ $GPR_USER ]; then
+    echo "gpr.user = $GPR_USER" > ~/.gradle/gradle.properties
+fi
+if [ $GPR_KEY ]; then
+    echo "gpr.key = $GPR_KEY" > ~/.gradle/gradle.properties
+fi
